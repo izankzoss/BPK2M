dd6355cef3796f97b788e5a9fb3bdb02d4a4e403
M. saiful Rizal <saifulrizal.1209@gmail.com>
2022-07-19 14:43:11 +0700

    Update asset_controllers.php

--- file/asset/controllers/asset_controllers.php
+++ file/asset/controllers/asset_controllers.php
@@ -2,16 +2,67 @@
 $db = __database();
 $opsi = $_GET['action'];
 //start input
-if($opsi == "input"){
+if ($opsi == "input") {
     $data = [
         'no_asset' => $_POST['no_asset'],
-        'id_pemusnahan' => $_POST['id_pemusnahan'],
-        'id_asset' => $_POST['id_asset'],
-        'username' => $_POST['username'],
-        'username' => $_POST['username'],
-        'username' => $_POST['username'],
-        'username' => $_POST['username'],
-        'username' => $_POST['username'],
-        
-    ]
+        'nm_barang' => $_POST['nm_barang'],
+        'jumlah' => $_POST['spesifikasi'],
+        'harga' => $_POST['harga'],
+        'total' => $_POST['total'],
+        'tgl_terima' => $_POST['username']
+    ];
+    $simpan = __simpan($db, "asset", $data);
+    if ($simpan) {
+?>
+        <script>
+            window.location.href = 'admin.php?target=asset';
+        </script>
+    <?php
+    } else {
+        echo "gagal simpan" . $db->error;
+    }
 }
+//end kondisi input
+//start kondisi delete
+elseif ($opsi = "delete") {
+    $where = [
+        'no_asset' => $_GET['id']
+    ];
+    $delete = __delete($db, "asset", $where);
+    if ($delete) {
+    ?>
+        <script>
+            window.location.href = 'admin.php?target=asset';
+        </script>
+    <?php
+    } else {
+        echo "gagal hapus" . $db->error;
+    }
+}
+
+//end kondisi delete
+//start update
+elseif ($opsi == "update") {
+    $data = [
+        'nm_barang' => $_POST['nm_barang'],
+        'jumlah' => $_POST['spesifikasi'],
+        'harga' => $_POST['harga'],
+        'total' => $_POST['total'],
+        'tgl_terima' => $_POST['tgl_terima']
+    ];
+    $where = [
+        'no_asset' => $_POST['id']
+    ];
+    $update = __update($db, "asset", $data, $where);
+    if ($update) {
+    ?>
+        <script>
+            window.location.href = 'admin.php?target=asset';
+        </script>
+<?php
+    } else {
+        echo "gagal update" . $db->error;
+    }
+}
+//end update
+?>
