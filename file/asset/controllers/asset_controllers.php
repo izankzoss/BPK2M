2f6f2b2f5be726f6a4d6064ca744b9f375af173f
M. saiful Rizal <saifulrizal.1209@gmail.com>
2022-07-19 14:14:30 +0700

    2

---
+++ file/asset/controllers/asset_controllers.php
@@ -0,0 +1,17 @@
+<?php
+$db = __database();
+$opsi = $_GET['action'];
+//start input
+if($opsi == "input"){
+    $data = [
+        'no_asset' => $_POST['no_asset'],
+        'id_pemusnahan' => $_POST['id_pemusnahan'],
+        'id_asset' => $_POST['id_asset'],
+        'username' => $_POST['username'],
+        'username' => $_POST['username'],
+        'username' => $_POST['username'],
+        'username' => $_POST['username'],
+        'username' => $_POST['username'],
+        
+    ]
+}
