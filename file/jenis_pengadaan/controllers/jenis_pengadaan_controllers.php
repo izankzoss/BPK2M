8d005c01b074b0b0c9369b5eb7648aa1c9fe3906
widad0910 <ahmadbadrimw02@gmail.com>
2022-07-19 16:55:30 +0700

    Update jenis_pengadaan_controllers.php

--- file/jenis_pengadaan/controllers/jenis_pengadaan_controllers.php
+++ file/jenis_pengadaan/controllers/jenis_pengadaan_controllers.php
@@ -5,15 +5,35 @@ $opsi = $_GET['action'];
 if ($opsi == "input") {
     $data = [
         'nama_barang' => $_POST['nama_barang'],
-        'tanggal_pengadaan' => $_POST['tanggal_pengadaan'],
         'jumlah_pengadaan' => $_POST['jumlah_pengadaan'],
         'sumber_pengadaan' => $_POST['sumber_pengadaan']
     ];
-    $simpan = __simpan(Sdb, "pengadaan", $data);
+    $simpan = __simpan($db, "pengadaan", $data);
     if ($simpan) {
 ?>
         <script>
             window.location.href = 'admin.php?target=pengadaan';
         </script>
+    <?php
+    } else {
+        echo "gagal simpan " . $db->error;
     }
 }
+//end input
+//start delete
+elseif ($opsi == "delete") {
+    $where = [
+        'nama_barang' => $_GET['id']
+    ];
+    $delete = __delete($db, "pengadaan", $where);
+    if ($delete) {
+    ?>
+        <script>
+            window.location.href = 'admin.php?target=pengadaan';
+        </script>
+    <?php
+    } else {
+        echo "gagal hapus" . $db->error;
+    }
+}
+//end delete
