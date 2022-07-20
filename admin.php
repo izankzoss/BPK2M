956de22b2b66b11f4b8f61fa39754906bef6ce4a
Zulfan Syafiqi <M-Zulfan@github.com>
2022-07-20 10:41:10 +0700

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+include "config/config_url.php";
+include "config/config_database.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to- fit=no">
+<title>Aplikasi BPK2M</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://cdn.datables.net/1.12.1/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="#">Aplikasi BPK2M</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data- target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria- expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="<?php echo base_url(); ?>">Home</a> 
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo isset($_GET['target']) && $_GET['target'] == 'cara' ? "active" : ""; ?>" href="<?php echo base_url(); ?>admin.php?target=cara">
+                        <i class="fa-solid fa-book"></i>Cara</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo isset($_GET['target']) && $_GET['target'] == 'inspeksi' ? "active" : ""; ?>" href="<?php echo base_url(); ?>admin.php?target=inspeksi">
+                            <i class="fa-solid fa-graduation-cap"></i>Inspeksi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo isset($_GET['target']) && $_GET['target'] == 'jenis' ? "active" : ""; ?>" href="<?php echo base_url(); ?>admin.php?target=jenis">
+                            <i class="fa-solid fa-building-columns"></i>Jenis</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo isset($_GET['target']) && $_GET['target'] == 'jenis asset' ? "active" : ""; ?>" href="<?php echo base_url(); ?>admin.php?target=jenis asset">
+                        <i class="fa-solid fa-book"></i>Jenis Asset</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo isset($_GET['target']) && $_GET['target'] == 'keadaan' ? "active" : ""; ?>" href="<?php echo base_url(); ?>admin.php?target=keadaan">
+                            <i class="fa-solid fa-graduation-cap"></i>Keadaan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo isset($_GET['target']) && $_GET['target'] == 'no registrasi' ? "active" : ""; ?>" href="<?php echo base_url(); ?>admin.php?target=no registrasi">
+                            <i class="fa-solid fa-building-columns"></i>No Registrasi</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo isset($_GET['target']) && $_GET['target'] == 'pemusnahan' ? "active" : ""; ?>" href="<?php echo base_url(); ?>admin.php?target=pemusnahan">
+                        <i class="fa-solid fa-book"></i>Pemusnahan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo isset($_GET['target']) && $_GET['target'] == 'pengadaan' ? "active" : ""; ?>" href="<?php echo base_url(); ?>admin.php?target=pengadaan">
+                            <i class="fa-solid fa-graduation-cap"></i>Pengadaan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo isset($_GET['target']) && $_GET['target'] == 'perbaikan' ? "active" : ""; ?>" href="<?php echo base_url(); ?>admin.php?target=perbaikan">
+                            <i class="fa-solid fa-building-columns"></i>Perbaikan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo isset($_GET['target']) && $_GET['target'] == 'sebab' ? "active" : ""; ?>" href="<?php echo base_url(); ?>admin.php?target=sebab">
+                        <i class="fa-solid fa-book"></i>Sebab</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo isset($_GET['target']) && $_GET['target'] == 'tempat' ? "active" : ""; ?>" href="<?php echo base_url(); ?>admin.php?target=tempat">
+                            <i class="fa-solid fa-graduation-cap"></i>Tempat</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+<div class="clearfix">&nbsp;</div>
+<div class="container">
+<?php include_once "content.php"; ?>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js">
+</script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0- beta1/dist/js/bootstrap.min.js"></script>
+</body>
+
+</html>
