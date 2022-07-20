943c0b7fbc29910888c098f123608879762119b3
asepkr7 <asepkhabril20@gmail.com>
2022-07-20 16:59:01 +0700

    Update list_inspeksi.php

---
+++ file/inspeksi/views/list_inspeksi.php
@@ -0,0 +1,36 @@
+  <?php 
+    $db = __database();
+    // buat header table
+    echo "<a class='btn btn-primary btn-sm' href='admin.php?
+target=insepeksi&action=form'><i class='fa-solid fa-plus'></i>Tambah</a><br><br>";
+      echo "<div class='table-responsive'>";
+      echo "<table class='table table-striped table-bordered'>
+      <thead>
+      <tr>
+      <th>No</th><th>Kode Prodi</th><th>Nama Prodi</th><th>#</th>
+      </tr>
+      </thead>
+      <tbody>";
+      // ambil data dari database
+      $q = __ambil($db, "inspeksi");
+      $no = 1;
+      while ($r = $q->fetch_array()) {
+        echo "<tr>
+       <td>" . $r['no_asset'] . "</td>
+        <td>" . $r['id_inspeksi'] . "</td>
+        <td>" . $r['tgl_inspeksi'] . "</td>
+        <td>" . $r['pelaksana'] . "</td>
+        <td>
+            <a class='btn btn-success btn-sm' href='admin.php?target=inspeksi&action=edit&id=" . $r['id_inspeksi'] . "'>
+            <i class='fa-solid fa-pen-to-square'></i>
+            Edit</a> 
+        <a class='btn btn-danger btn-sm' href='admin.php?target=inspeksi&action=delete&id=" . $r['id_inspeksi'] . "'>
+        <i class='fa-solid fa-trash'></i>
+        Hapus</a>
+            </td>
+            </tr>";
+            $no++;
+      }
+      echo "</tbody></table></div>"; ?>
+  </div>
+</div>
