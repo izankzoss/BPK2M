111d3ac1c045a235043319ea82a704a2a2bc4ebc
asepkr7 <asepkhabril20@gmail.com>
2022-07-21 13:52:53 +0700

    Update form_inspeksi.php

--- file/inspeksi/views/form_inspeksi.php
+++ file/inspeksi/views/form_inspeksi.php
@@ -1,36 +1,52 @@
-<?php
-$db = __database();
-?>
 <div class="card">
-    <div class="card-header">
-        <h4>Form Inspeksi</h4>
+  <div class="card-header">
+    <h4>Form Inspeksi</h4>
     </div>
     <div class="card-body">
-    <form method="post" action="admin.php?target=inspeksi&action=input" data-parsley-validate class="form-horizontal form-label-left">
-    <div class="mb-3">
-            <label class="control-label col-md-3 col-sm-3 col-lg-12" for="first-name">
-               Tanggal
-            </label>
-            <div class="col-md-6 col-sm-6 col-lg-12">
-                <input type="text" name="tanggal" class="form-control" />
-            </div> 
-        </div> 
-    <div class="mb-3">
-            <label class="control-label col-md-3 col-sm-3 col-lg-12" for="first-name">
-                Pengadaan
-            </label>
-            <div class="col-md-6 col-sm-6 col-lg-12">
-                <input type="text" name="pengadaan" class="form-control" />
-            </div> 
+      <form method="post" action="admin.php?target=prodi&action=input" data-parsley-validate
+       class="form-horizontal form-label-left">
+       <div class="mb-3">
+        <label class="control-label col-md-3 col-sm-3 col-lg-12" for="first-name">
+ No Asset
+ </label>
+ <div class="col-md-6 col-sm-6 col-lg-12">
+  <input type="text" name="no_asset" class="form-control" />
+       </div>
+       </div>
+       <div class="mb-3">
+        <label for="first-name" class="control-label col-md-3 col-sm-3 col-lg-12">
+          Id Inspeksi
+        </label>
+        <div class="col-md-6 col-sm-6 col-lg-12 ">
+          <input type="text" name="id_inspeksi" class="form-control">
         </div>
-            <div class="mb-3">
-                <div class="col-md-6 col-sm-6 col-lg-12">
-                    <button type="submit" class="btn btn-success btn-sm" id="simpan" name="simpan">
-                        <i class="fa-solid fa-floppy-disk"></i> Simpan
-                    </button>
-                    <a class="btn btn-danger btn-sm" href="admin.php?target=inspeksi"><i class="fa-solid fa-circle-arrow-left"></i>Kembali</a>
-                </div>
+       </div>
+       <div class="mb-3">
+        <label for="first-name" class="control-label col-md-3 col-sm-3 col-lg-12">
+          Id Keadaan
+        </label>
+        <div class="col-md-6 col-sm-6 col-lg-12">
+          <input type="text" name="id_keadaan" class="form-comtrol">
+        </div>
+       </div>
+        <div class="mb-3">
+        <label for="first-name" class="control-label col-md-3 col-sm-3 col-lg-12">
+          Pelaksana
+        </label>
+        <div class="col-md-6 col-sm-6 col-lg-12 ">
+          <input type="text" name="Pelaksana" class="form-control">
+        </div>
+       </div>
+       <div class="mb-3">
+       <div class="col-md-6 col-sm-6 col-lg-12">
+          <button type="submit" class="btn btn-success btn-sm" id="simpan" name="Simpan"> <i class="fa-solid fa-floppy-disk"></i> 
+        </button>
+          <a class="btn btn-danger btn-sm" href="admin.php?target=prodi"><i class="fa-solid fa-circle-arrow-left"></i>
+         Kembali</a>
             </div>
-        </form>
-    </div>
-</div>
+           </div>
+           </form>
+          </div>
+        </div>
+
+
