497d95e39da749242015f2f7bdefccc114ce5614
M. saiful Rizal <saifulrizal.1209@gmail.com>
2022-07-23 20:49:32 +0700

    Update list_inspeksi.php

--- file/inspeksi/views/list_inspeksi.php
+++ file/inspeksi/views/list_inspeksi.php
@@ -1,12 +1,12 @@
   <div class="card">
-  <div class="card-header">
-    <h4><i class="fa-solid fa-database"></i> Data Inspeksi</h4>
-  </div>
-  <div class="card-body">
-  <?php 
-    $db = __database();
-    // buat header table
-    echo "<a class='btn btn-primary btn-sm' href='admin.php?
+    <div class="card-header">
+      <h4><i class="fa-solid fa-database"></i> Data Inspeksi</h4>
+    </div>
+    <div class="card-body">
+      <?php
+      $db = __database();
+      // buat header table
+      echo "<a class='btn btn-primary btn-sm' href='admin.php?
 target=inspeksi&action=form'><i class='fa-solid fa-plus'></i>Tambah</a><br><br>";
       echo "<div class='table-responsive'>";
       echo "<table class='table table-striped table-bordered'>
@@ -35,10 +35,8 @@ target=inspeksi&action=form'><i class='fa-solid fa-plus'></i>Tambah</a><br><br>"
         Hapus</a>
             </td>
             </tr>";
-            $no++;
+        $no++;
       }
       echo "</tbody></table></div>"; ?>
+    </div>
   </div>
-</div>
-
-       
