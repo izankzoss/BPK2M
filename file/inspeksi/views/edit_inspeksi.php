a3fc81dba8ead8fcacda20cdfb83d37723a02376
Zulfan Syafiqi <M-Zulfan@github.com>
2022-07-23 22:15:59 +0700

    Update edit_inspeksi.php

--- file/inspeksi/views/edit_inspeksi.php
+++ file/inspeksi/views/edit_inspeksi.php
@@ -4,9 +4,11 @@ $where = [
     'id_inspeksi' => $_GET['id']
 ];
 $query  = __ambil($db, "inspeksi", "*", $where);
+// menampilkan hasil query dalam bentuk object
+// anda bisa juga menggunakan mysql_fetch_assoc atau mysql_fetch_array dll
 
 $rows      = $query->fetch_object();
-
+// print_r($rows);
 ?>
 <div class="card">
     <div class="card-header">
@@ -14,13 +16,13 @@ $rows      = $query->fetch_object();
     </div>
     <div class="card-body">
         <form method="post" action="admin.php?target=inspeksi&action=update" data-parsley-validate class="form-horizontal form-label-left">
-            <input type="hidden" name="id" value="<?php echo $rows->kd_prodi; ?>">
+            <input type="hidden" name="id" value="<?php echo $rows->id_inspeksi; ?>">
             <div class="mb-3">
                 <label class="control-label col-md-3 col-sm-3 col-lg-12" for="first-name">
                     Pengadaan
                 </label>
                 <div class="col-md-6 col-sm-6 col-lg-12">
-                    <input type="text" id="id_inspeksi" name="tgl_inspeksi" value="<?php echo $rows->pengadaan; ?>" required="requerid" class="form-control col-md-7 col-xs-12">
+                    <input type="text" id="id_inspeksi" name="pengadaan" value="<?php echo $rows->pengadaan; ?>" required="requerid" class="form-control col-md-7 col-xs-12">
                 </div>
             </div>
             <div class="mb-3">
