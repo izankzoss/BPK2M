82e15379b789caa0655ff5e75e4cee2a9af23cfb
Zulfan Syafiqi <M-Zulfan@github.com>
2022-07-23 22:15:55 +0700

    Update inspeksi_controllers.php

--- file/inspeksi/controllers/inspeksi_controllers.php
+++ file/inspeksi/controllers/inspeksi_controllers.php
@@ -1,69 +1,61 @@
-<?php 
+<?php
 $db = __database();
 $opsi = $_GET['action'];
-// star input
+// start input
 if ($opsi == "input") {
-  $data = [
-    'id_inspeksi' => $_POST['inspeksi'],
-    'id_keadaan' => $_POST['id_keadaan'],
-    'no_asset' => $_POST['no_asset'],
-    'tgl_inspeksi' => $_POST['tgl_inspeksi'],
-    'pelaksana' => $_POST['pelaksana']
-  ];
-$simpan = __simpan($db, "inspeksi", $data);
-if ($simpan) {
+    $data = [
+        
+        'tgl_inspeksi' => $_POST['tgl_inspeksi'],
+        'pengadaan' => $_POST['pengadaan']
+    ];
+    $simpan = __simpan($db, "inspeksi", $data);
+    if ($simpan){
 ?>
-<script>
-  window.location.href = 'admin.php?terget=inspeksi';
-</script>
-<?php 
-} else {
-  echo "gagal simpan " .$db->error;
-}
+        <script>
+            window.location.href = 'admin.php?target=inspeksi';
+        </script>
+    <?php
+    } else {
+        echo "gagal simpan " . $db->error;
+    }
 }
 // end kondisi input
 // start kondisi delete
 elseif ($opsi == "delete") {
-  $where = [
-    'id_inspeksi' => $_GET['id']
-  ];
- $delete = __delete($db, "inspeksi", $where);
- if ($delete) {
-?>
-<script>
-  window.location.href = 'admin.php?terget=inspeksi';
-</script>
-<?php 
- } else {
-  echo "gagal hapus " . $db->error;
-
- }
-
-} 
+    $where = [
+        'tgl_inspeksi' => $_GET['id']
+    ];
+    $delete = __delete($db, "inspeksi", $where);
+    if ($delete) {
+    ?>
+        <script>
+            window.location.href = 'admin.php?target=inspeksi';
+        </script>
+    <?php
+    } else {
+        echo "gagal hapus" . $db->error;
+    }
+}
 // end kondisi delete
 // start kondisi update
-
-elseif ($opsi == "update") { 
+elseif ($opsi == "update") {
     $data = [
-       'id_keadaan' => $_POST['id_keadaan'],
-      'no_asset' => $_POST['no_asset'],
-      'tgl_inspeksi' => $_POST['tgl_inspeksi'],
-    'pelaksana' => $_POST['pelaksana']
-
+        'pengadaan' => $_POST['pengadaan']
     ];
-      $where = [
-        'id_inspeksi' => $_POST['id']
-      ];
-      $update = __update($db, "inspeksi", $data, $where); 
-      if ($update) {
-        ?>
+    $where = [
+        'tgl_inspeksi' => $_POST['id']
+    ];
+    $update = __update($db, "inspeksi", $data, $where);
+    if ($update) {
+
+    ?>
         <script>
-        window.location.href ='admin.php?target=inspeksi';
-      </script>
-      <?php
-  } else {
-    echo "gagal update" . $db -> error;
-  }
+            window.location.href = 'admin.php?target=inspeksi';
+        </script>
+<?php
+    } else {
+        echo "gagal update" . $db->error;
+    }
 }
-// end kondisi update 
+// end kondisi update
 ?>
