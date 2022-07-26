aa6201c4af29177b163e2e671c848b241a1b5721
M. saiful Rizal <saifulrizal.1209@gmail.com>
2022-07-26 22:14:49 +0700

    Update perbaikan_controllers.php

---
+++ file/perbaikan/controllers/perbaikan_controllers.php
@@ -0,0 +1,66 @@
+<?php
+$db = __database();
+$opsi = $_GET['action'];
+//start input
+if ($opsi == "input") {
+    $data = [
+        'id_perbaikan' => $_POST['id_perbaikan'],
+        'perbaikan' => $_POST['perbaikan'],
+        'ket_perbaikan' => $_POST['ket_perbaikan'],
+        'tgl_perbaikan' => $_POST['tgl_perbaikan'],
+        'biaya' => $_POST['biaya']
+    ];
+    $simpan = __simpan($db, "perbaikan", $data);
+    if ($simpan) {
+?>
+        <script>
+            window.location.href = 'admin.php?target=perbaikan';
+        </script>
+    <?php
+    } else {
+        echo "gagal simpan" . $db->error;
+    }
+}
+//end kondisi input
+//start kondisi delete
+elseif ($opsi == "delete") {
+    $where = [
+        'id_perbaikan' => $_GET['id']
+    ];
+    $delete = __delete($db, "perbaikan", $where);
+    if ($delete) {
+    ?>
+        <script>
+            window.location.href = 'admin.php?target=perbaikan';
+        </script>
+    <?php
+    } else {
+        echo "gagal hapus" . $db->error;
+    }
+}
+
+//end kondisi delete
+//start update
+elseif ($opsi == "update") {
+    $data = [
+        'perbaikan' => $_POST['perbaikan'],
+        'ket_perbaikan' => $_POST['ket_perbaikan'],
+        'tgl_perbaikan' => $_POST['tgl_perbaikan'],
+        'biaya' => $_POST['biaya']
+    ];
+    $where = [
+        'id_cara' => $_POST['id']
+    ];
+    $update = __update($db, "perbaikan", $data, $where);
+    if ($update) {
+    ?>
+        <script>
+            window.location.href = 'admin.php?target=perbaikan';
+        </script>
+<?php
+    } else {
+        echo "gagal update" . $db->error;
+    }
+}
+//end update
+?>
