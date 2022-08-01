67b4ceab025cb5f08f39b0bca2f248ab78c9ded6
M. saiful Rizal <saifulrizal.1209@gmail.com>
2022-08-01 13:42:11 +0700

    Update content.php

--- content.php
+++ content.php
@@ -2,18 +2,69 @@
 
 if (!isset($_GET['target'])) {
 ?>
-    <div class="col-md-12 col-sm-12 col-xs-12">
-        <div class="x_panel">
-            <div class="x_title">
-                <h2>Halaman Utama</h2>
-                <div class="clearfix"></div>
+    <div class="row">
+        <?php
+        $db = __database();
+        $q = __ambil($db, "asset");
+        $p = __ambil($db, "inspeksi");
+        $s = __ambil($db, "pengadaan");
+        $n = __ambil($db, "pemusnahan");
+        while ($r = $q->fetch_array()) {
+            $total = mysqli_num_rows($q);
+            $total2 = mysqli_num_rows($p);
+            $total1 = mysqli_num_rows($s);
+            $total3 = mysqli_num_rows($n);
+        } ?>
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3><?php echo $total; ?></h3>
+                    <p>Asset</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-user"></i>
+                </div>
+                <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
-            <div class="x_content">
-                Selamat Datang di Aplikasi BPK2M
-                <br><br><br><br><br><br><br><br><br><br><br><br><br>
-                Ip Adress Anda: <?php echo $_SERVER['REMOTE_ADDR']; ?>
-                <br>
-                Tanggal : <?php echo date("d/m/Y"); ?>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3><?php echo $total2; ?></h3>
+                    <p>Inspeksi</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-user"></i>
+                </div>
+                <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3><?php echo $total1; ?></h3>
+                    <p>Pengadaan</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-user"></i>
+                </div>
+                <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+            <!-- small box -->
+            <div class="small-box bg-aqua">
+                <div class="inner">
+                    <h3><?php echo $total3; ?></h3>
+                    <p>Pemusnahan</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-user"></i>
+                </div>
+                <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
     </div>
