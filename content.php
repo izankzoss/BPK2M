6967ba14f0c120f644f77a95d93836bbe73e6545
M. saiful Rizal <saifulrizal.1209@gmail.com>
2022-08-01 14:05:08 +0700

    Update content.php

--- content.php
+++ content.php
@@ -15,74 +15,78 @@ if (!isset($_GET['target'])) {
             $total1 = mysqli_num_rows($s);
             $total3 = mysqli_num_rows($n);
         } ?>
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-green">
-                <div class="inner">
-                    <h3><?php echo $total; ?></h3>
-                    <p>Asset</p>
+        <section class="content">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                            <h3><?php echo $total; ?></h3>
+                            <p>Asset</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
                 </div>
-                <div class="icon">
-                    <i class="fa fa-user"></i>
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-aqua">
+                        <div class="inner">
+                            <h3><?php echo $total2; ?></h3>
+                            <p>Inspeksi</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-cube"></i>
+                        </div>
+                        <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
                 </div>
-                <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-aqua">
+                        <div class="inner">
+                            <h3><?php echo $total1; ?></h3>
+                            <p>Pengadaan</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-xs-6">
+                    <!-- small box -->
+                    <div class="small-box bg-aqua">
+                        <div class="inner">
+                            <h3><?php echo $total3; ?></h3>
+                            <p>Pemusnahan</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-user"></i>
+                        </div>
+                        <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-aqua">
-                <div class="inner">
-                    <h3><?php echo $total2; ?></h3>
-                    <p>Inspeksi</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-user"></i>
-                </div>
-                <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-aqua">
-                <div class="inner">
-                    <h3><?php echo $total1; ?></h3>
-                    <p>Pengadaan</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-user"></i>
-                </div>
-                <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        <div class="col-lg-3 col-xs-6">
-            <!-- small box -->
-            <div class="small-box bg-aqua">
-                <div class="inner">
-                    <h3><?php echo $total3; ?></h3>
-                    <p>Pemusnahan</p>
-                </div>
-                <div class="icon">
-                    <i class="fa fa-user"></i>
-                </div>
-                <a href="karyawan.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-    </div>
-    <?php
-} else {
-    $target = $_GET['target'];
-    if (empty($target)) {
-    ?>
-        <script>
-            window.location.href = 'admin.php';
-        </script>
-<?php
-    }
-
-    if (!isset($_GET['action'])) {
-        getContentAdmin(base_url(), $target);
+        </section>
+        <?php
     } else {
-        getContentAdmin(base_url(), $target, $_GET['action']);
+        $target = $_GET['target'];
+        if (empty($target)) {
+        ?>
+            <script>
+                window.location.href = 'admin.php';
+            </script>
+    <?php
+        }
+
+        if (!isset($_GET['action'])) {
+            getContentAdmin(base_url(), $target);
+        } else {
+            getContentAdmin(base_url(), $target, $_GET['action']);
+        }
     }
-}
-?>
+    ?>
