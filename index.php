e16c7f01ec4eef9c75c6ce187f0531c9c31c38b1
asepkr7 <asepkhabril20@gmail.com>
2022-08-02 10:19:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
-if ($_SESSION['status_login' == true]) {
-    header('location"admin.php');
-} else {
-    header('location:login.php');
+if ($_SESSION['masuk']='admin') {
+    header('location:admin.php');
+} elseif($_SESSION['masuk']='user') {
+    header('location:users.php');
+    
+}
+else{
+header('location:login.php');
 }
