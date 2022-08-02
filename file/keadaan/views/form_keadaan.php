d9c770b77e660ab9eaa03e92324c1b0589b235ad
Zulfan Syafiqi <M-Zulfan@github.com>
2022-08-02 22:21:06 +0700

    Update form_keadaan.php

--- file/keadaan/views/form_keadaan.php
+++ file/keadaan/views/form_keadaan.php
@@ -15,11 +15,22 @@
                 </div>
             </div>
             <div class="mb-3">
-                <label for="control-label col-md-3 col-sm-3 col-lg-12" for="first-name">
+                <label class="control-label col-md-3 col-sm-3 col-lg-12" for="first-name">
                     Keadaan
                 </label>
                 <div class="col-md-6 col-sm-6 col-lg-12">
-                    <input type="text" name="keadaan" class="form-control" />
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" name="keadaan" id="keadaan_baik" value="baik" <?php echo $rows->KEADAAN == "baik" ? "checked" : ""; ?>>
+                        <label for="keadaan_baik" class="form-check-label">
+                            Baik
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input type="radio" class="form-check-input" name="keadaan" id="keadaan_rusak" value="rusak" <?php echo $rows->KEADAAN == "rusak" ? "checked" : ""; ?>>
+                        <label for="keadaan_rusak" class="form-check-label">
+                            Rusak
+                        </label>
+                    </div>
                 </div>
             </div>
             <div class="mb-3">
