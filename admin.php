bd2e37f5d52e1b9668380ae5502c453ba52820bb
Zulfan Syafiqi <M-Zulfan@github.com>
2022-08-02 12:45:34 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -249,12 +249,12 @@ include "config/config_database.php";
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Siap untuk Meninggalkan?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
                 </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-body">Pilih "Keluar" di bawah jika Anda siap untuk mengakhiri sesi Anda saat ini.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Kembali</button>
                     <a class="btn btn-primary" href="login.php">Keluar</a>
