44711d10ca6229071de52999b0b1106e5290eb27
M. saiful Rizal <saifulrizal.1209@gmail.com>
2022-08-03 13:01:40 +0700

    ..

--- file/asset/views/edit_asset.php
+++ file/asset/views/edit_asset.php
@@ -14,14 +14,14 @@ $rows = $query->fetch_object();
         <h4>Edit Asset</h4>
     </div>
     <div class="card-body">
-        <input type="text" name="id" value="<?php echo $rows->NO_ASSET; ?>">
         <form method="POST" action="admin.php?target=asset&action=update" data-parsley-validate class="form-horizontal form-label-left">
+            <input type="text" name="id" value="<?php echo $rows->NO_ASSET; ?>">
             <div class="mb-3">
                 <label class="control-label col-md-3 col-sm-3 col-lg-12" for="first-name">
                     Nama Barang
                 </label>
                 <div class="col-md-6 col-sm-6 col-lg-12">
-                    <input id="nm_barang" type="text" name="NM_BARANG" class="form-control" value="<?php echo $rows->NM_BARANG; ?>" required="required" class="form-control col-md-7 col-xs-12">
+                    <input id="nm_barang" type="text" name="nm_barang" class="form-control" value="<?php echo $rows->nm_barang; ?>" required="required" class="form-control col-md-7 col-xs-12">
                 </div>
             </div>
             <div class="mb-3">
@@ -30,13 +30,13 @@ $rows = $query->fetch_object();
                 </label>
                 <div class="col-md-6 col-sm-6 col-lg-12">
                     <div class="form-check">
-                        <input type="radio" class="form-check-input" name="SPESIFKASI" id="spesifikasi_baru" value="baru" <?php echo $rows->SPESIFIKASI == "Baru" ? "checked" : ""; ?>>
+                        <input type="radio" class="form-check-input" name="spesifikasi" id="spesifikasi_baru" value="baru" <?php echo $rows->SPESIFIKASI == "Baru" ? "checked" : ""; ?>>
                         <label for="spesifikasi_baru" class="form-check-label">
                             Baru
                         </label>
                     </div>
                     <div class="form-check">
-                        <input type="radio" class="form-check-input" name="SPESIFKASI" id="spesifikasi_bekas" value="bekas" <?php echo $rows->SPESIFIKASI == "Bekas" ? "checked" : ""; ?>>
+                        <input type="radio" class="form-check-input" name="spesifikasi" id="spesifikasi_bekas" value="bekas" <?php echo $rows->SPESIFIKASI == "Bekas" ? "checked" : ""; ?>>
                         <label for="spesifikasi_bekas" class="form-check-label">
                             Bekas
                         </label>
@@ -48,7 +48,7 @@ $rows = $query->fetch_object();
                     Jumlah
                 </label>
                 <div class="col-md-6 col-sm-6 col-lg-12">
-                    <input id="jumlah" type="text" name="JUMLAH" class="form-control" value="<?php echo $rows->JUMLAH; ?>" class="form-control col-md-7 col-xs-12">
+                    <input id="jumlah" type="text" name="jumlah" class="form-control" value="<?php echo $rows->jumlah; ?>" class="form-control col-md-7 col-xs-12">
                 </div>
             </div>
             <div class="mb-3">
@@ -56,7 +56,7 @@ $rows = $query->fetch_object();
                     Harga
                 </label>
                 <div class="col-md-6 col-sm-6 col-lg-12">
-                    <input id="jumlah" type="text" name="HARGA" class="form-control" value="<?php echo $rows->HARGA; ?>" class="form-control col-md-7 col-xs-12">
+                    <input id="jumlah" type="text" name="harga" class="form-control" value="<?php echo $rows->harga; ?>" class="form-control col-md-7 col-xs-12">
                 </div>
             </div>
             <div class="mb-3">
@@ -64,13 +64,13 @@ $rows = $query->fetch_object();
                     Instansi
                 </label>
                 <div class="col-md-6 col-sm-6 col-lg-12">
-                    <select name="ID_INSTANSI" id="ID_INSTANSI" class="form-select">
+                    <select name="id_instansi" id="id_instansi" class="form-select">
                         <option value="">Pilih Instansi</option>
                         <?php
                         $instansi_data = __ambil($db, "instansi", "*");
                         while ($r = $instansi_data->fetch_array()) {
                         ?>
-                            <option value="<?php echo $r['ID_INSTANSI']; ?>" <?php echo $rows->ID_INSTANSI == $r['NM_INSTANSI'] ? "selected" : ""; ?>> <?php echo $r['NM_INSTANSI']; ?> </option>
+                            <option value="<?php echo $r['ID_INSTANSI']; ?>" <?php echo $rows->ID_INSTANSI == $r['ID_INSTANSI'] ? "selected" : ""; ?>> <?php echo $r['NM_INSTANSI']; ?> </option>
                         <?php
                         }
                         ?>
@@ -82,13 +82,13 @@ $rows = $query->fetch_object();
                     Jenis Asset
                 </label>
                 <div class="col-md-6 col-sm-6 col-lg-12">
-                    <select name="ID_JENIS" id="ID_JENIS" class="form-select">
+                    <select name="id_asset" id="id_asset" class="form-select">
                         <option value="">Pilih Jenis Asset</option>
                         <?php
                         $jenis_data = __ambil($db, "jenis_asset", "*");
-                        while ($k = $jenis_data->fetch_array()) {
+                        while ($r = $jenis_data->fetch_array()) {
                         ?>
-                            <option value="<?php echo $k['ID_ASSET']; ?>" <?php echo $rows->ID_ASSET == $k['JENIS_ASSET'] ? "selected" : ""; ?>> <?php echo $k['JENIS_ASSET']; ?> </option>
+                            <option value="<?php echo $r['ID_ASSET']; ?>" <?php echo $rows->id_asset == $r['id_asset'] ? "selected" : ""; ?>> <?php echo $r['JENIS_ASSET']; ?> </option>
                         <?php
                         }
                         ?>
